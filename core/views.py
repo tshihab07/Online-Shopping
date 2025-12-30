d536f3c16bdca0a2e7c0b105f2e45d249a5543fa
tshihab07 <tushar.shihab13@gmail.com>
2025-12-30 23:59:31 +0600

    feat: add initial view functions for core apps

--- core/views.py
+++ core/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'signup.html')
