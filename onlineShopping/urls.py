2040b7548fbe316be7942817b60fb247c0bfdac7
tshihab07 <tushar.shihab13@gmail.com>
2025-12-30 23:59:01 +0600

    feat: define core application routes

--- onlineShopping/urls.py
+++ onlineShopping/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
+from core.views import index, contact, login, register
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('signup/', register, name='register'),
     path('admin/', admin.site.urls),
 ]
