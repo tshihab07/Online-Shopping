764d32d7ffd089d6faf6f2afc541cd585c6eb134
tshihab07 <tushar.shihab13@gmail.com>
2026-01-03 22:00:51 +0600

    feat: add item to the installed apps

--- onlineShopping/settings.py
+++ onlineShopping/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'item'
 ]
 
 STATIC_URL = '/static/'
