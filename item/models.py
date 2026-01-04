b4d2de052b3b87fde867d5d5e71e1c42905e2d19
tshihab07 <tushar.shihab13@gmail.com>
2026-01-04 23:56:10 +0600

    feat: add Item entity to support listing functionality

--- item/models.py
+++ item/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(255)
 
     class Meta:
+        ordering = ('name',)
         verbose_name_plural = "Categories"
     
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='product_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='items')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = "Items"
+
     def __str__(self):
         return self.name
