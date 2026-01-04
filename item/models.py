63e29f555c44e7ce47a74a287d9d03602ed6e663
tshihab07 <tushar.shihab13@gmail.com>
2026-01-04 23:36:07 +0600

    feat: define Category fields and metadata

--- item/models.py
+++ item/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(255)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
