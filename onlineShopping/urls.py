9f9a2aae0f5bce5028409495bb0066739f214289
tshihab07 <tushar.shihab13@gmail.com>
2026-01-05 23:49:22 +0600

    feat: configure media file routing in urls

--- onlineShopping/urls.py
+++ onlineShopping/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from core.views import index, contact, login, register
 
 urlpatterns = [
@@ -9,4 +12,4 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('signup/', register, name='signup'),           # refactor: name = register -> name = signup
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
