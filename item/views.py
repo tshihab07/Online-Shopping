b8691a38084e0a115a51faa0f38c722563e34b5a
tshihab07 <tushar.shihab13@gmail.com>
2026-01-08 01:17:08 +0600

    feat: add item detail view to item app

--- item/views.py
+++ item/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Item
 
-def item_detail(requeest, pk):
+def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     
-    return (requeest, "details.html",
+    return render (request, "details.html",
             {
               'item': item
             })
