b56835a75468afbc13c7a929aa0f319a21e0f8ae
tshihab07 <tushar.shihab13@gmail.com>
2026-01-08 15:30:56 +0600

    feat: add related_items queryset to show items from same category

--- item/views.py
+++ item/views.py
@@ -3,8 +3,10 @@ from .models import Item
 
 def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category).exclude(pk=pk)[:3]
     
     return render (request, "details.html",
             {
-              'item': item
+              'item': item,
+              'related_items': related_items
             })
